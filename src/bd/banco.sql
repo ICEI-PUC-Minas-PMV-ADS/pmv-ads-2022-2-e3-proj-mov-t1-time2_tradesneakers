2dbfcf6ceb27c65cc6895eb89536a5f2e02f278f
Talles Gois <tallesmonteiro0@gmail.com>
2022-10-09 14:19:57 -0300

    Adiciona tabelas intermediarias no modelo fisico e pequenas correções

--- src/bd/banco.sql
+++ src/bd/banco.sql
@@ -14,7 +14,7 @@ PRIMARY KEY(IdUsuario)
 CREATE TABLE Administrador(
 IdAdministrador INT NOT NULL,
 PRIMARY KEY(IdAdministrador)
-FOREIGN KEY (IdAdministrador) REFERENCES usuario(IdUsuario),
+FOREIGN KEY (IdAdministrador) REFERENCES Usuario(IdUsuario),
 );
 
 -- Cria a tabela de usuários comuns
@@ -24,7 +24,7 @@ IdUsuario INT NOT NULL,
 Nome varchar(50),
 Sobrenome varchar(50),
 PRIMARY KEY(IdUsuario)
-FOREIGN KEY (IdUsuario) REFERENCES usuario(IdUsuario),
+FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
 );
 
 -- Cria a tabela de endereço
@@ -37,7 +37,7 @@ Estado varchar(50),
 Pais varchar(50),
 Cep varchar(50),
 PRIMARY KEY (IdEndereco),
-FOREIGN KEY (IdUsuario) REFERENCES usuario(IdUsuario),
+FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
 );
 
 -- Cria a tabela de Mensagem
@@ -50,8 +50,8 @@ Assunto varchar(50),
 Conteudo varchar(50),
 VisualizadaOuNao NOT NULL,
 PRIMARY KEY (IdMensagem),
-FOREIGN KEY (IdRemetente) REFERENCES usuario(IdUsuario),
-FOREIGN KEY (IdDestinatario) REFERENCES usuario(IdUsuario),
+FOREIGN KEY (IdRemetente) REFERENCES Usuario(IdUsuario),
+FOREIGN KEY (IdDestinatario) REFERENCES Usuario(IdUsuario),
 );
 
 -- Cria a tabela de Pedido
@@ -65,8 +65,8 @@ DataEnvio varchar(50),
 PedidoFechado NOT NULL,
 PedidoEntregue NOT NULL,
 PRIMARY KEY (IdPedido),
-FOREIGN KEY (IdUsuario) REFERENCES usuario(IdUsuario),
-FOREIGN KEY (IdVendedor) REFERENCES usuario(IdUsuario),
+FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
+FOREIGN KEY (IdVendedor) REFERENCES Usuario(IdUsuario),
 );
 
 -- Cria a tabela de Produto
@@ -77,10 +77,10 @@ IdUsuario int NOT NULL,
 Nome varchar(50),
 Marca varchar(50),
 Numeracao int ,
-Descricao varchar(50),
+Descricao varchar(300),
 Preco float,
 PRIMARY KEY (IdProduto),
-FOREIGN KEY (IdUsuario) REFERENCES usuario(IdUsuario),
+FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
 );
 
 -- Cria a tabela de Foto
@@ -90,7 +90,7 @@ IdFotoProduto int NOT NULL,
 IdProduto int NOT NULL,
 UrlDaImage varchar(50),
 PRIMARY KEY (IdFotoProduto),
-FOREIGN KEY (IdProduto) REFERENCES produto(IdProduto)
+FOREIGN KEY (IdProduto) REFERENCES Produto(IdProduto)
 );
 
 -- Cria a tabela de Troca
@@ -100,9 +100,29 @@ IdProposta int NOT NULL,
 IdUsuario int NOT NULL,
 IdVendedor int NOT NULL,
 CustoEnvio float,
-Mensagem varchar(50),
+Mensagem varchar(500),
 PropostoAceitaOuNao NOT NULL,
 PRIMARY KEY (IdProposta),
 FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
-FOREIGN KEY (IdVendedor) REFERENCES usuario(IdUsuario),
+FOREIGN KEY (IdVendedor) REFERENCES Usuario(IdUsuario),
+);
+
+-- Cria tabela intermediaria Pedido e Produto
+
+CREATE TABLE Produto_pedido(
+IdProduto int NOT NULL,
+IdPedido int NOT NULL,
+PRIMARY KEY (IdProduto, IdPedido),
+FOREIGN KEY (IdPedido) REFERENCES Pedido(IdPedido),
+FOREIGN KEY (IdProduto) REFERENCES Produto(IdProduto,)
+);
+
+-- Cria tabela intermediaria Produto e Proposta_Troca
+
+CREATE TABLE Produto_Proposta_Troca(
+IdProduto int NOT NULL,
+IdProposta int NOT NULL,
+PRIMARY KEY (IdProduto, IdProposta),
+FOREIGN KEY (IdProduto) REFERENCES Produto(IdProduto,)
+FOREIGN KEY (IdProposta) REFERENCES Proposta_troca(IdProposta),
 );
